bbd6152a7e73e65228f8c82fde0a801698ba32d9
JoshTheConfused <97866996+JoshTheConfused@users.noreply.github.com>
2022-03-12 16:24:42 -0500

    Version 2 complete
    Sonnets have some English grammar now, still far from perfect however. Also a bug where lines have 11 syllables.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 
+from datetime import datetime
+
 from json import JSONDecodeError
 
 from os.path import exists
@@ -98,7 +100,7 @@ def make_rhyme_list(word):
         except AttributeError:
             continue
 
-    print(f"Returning {len(list_of_rhyming_words)} words rhyming with {word}")
+    # print(f"Returning {len(list_of_rhyming_words)} words rhyming with {word}")
     return list_of_rhyming_words
 
 
@@ -110,18 +112,18 @@ def safe_rhyme_word_index(rhyme_list):  # finds a rhyme that does not contain a 
         index = random.randrange(len(rhyme_list)) - 1
         out = rhyme_list[index]
         try:
-            unsafe = " " in out['word'] or out['word'] == "" or "n" not in out['tags'] and "v" not in \
+            unsafe = " " in out['word'] or out['word'] == "" or out['numSyllables'] > 8 or "n" not in out['tags'] and "v" not in \
                      out['tags'] and "adj" not in out['tags'] and "adv" not in out['tags'] and \
                      'tags' not in out
         except KeyError:
             continue
 
-    print(f"Found safe rhyme '{rhyme_list[index]}'")
+    # print(f"Found safe rhyme '{rhyme_list[index]}'")
     return index
 
 
 def rhyme_list_safe(rhyme_list):  # ensures that the list has at least one word without a space
-    print(f"verifying safety of list: {rhyme_list}")
+    # print(f"verifying safety of list: {rhyme_list}")
     if len(rhyme_list) == 0:
         return False
     for i in rhyme_list:
@@ -130,7 +132,7 @@ def rhyme_list_safe(rhyme_list):  # ensures that the list has at least one word 
         except KeyError:
             continue
 
-        if " " not in i['word'] and has_right_speech_part:
+        if " " not in i['word'] and has_right_speech_part and i['numSyllables'] < 9:
             return True
     return False
 
@@ -146,23 +148,278 @@ def write_line():
             break
         cycles += 1
 
-    print("chosen word: ", chosen_word)
+    # print("chosen word: ", chosen_word)
     chosen_word = write_line_input(chosen_word, False)
     return chosen_word  # allows word to be used to generate the next line
 
 
+def proper_line_word_chooser(part_of_speech, syllable_count, to_ten):
+    new_word = {'word': '', 'numSyllables': 10, 'tags': []}
+    n_1_syl = new_word['numSyllables']
+    tags = new_word['tags']
+
+    iterations = 0
+
+    if to_ten:
+        while n_1_syl + syllable_count > 10 or part_of_speech not in tags:
+            new_word = word_list_json[random.randrange(len(word_list_json)) - 1]
+            try:
+                n_1_syl = new_word['numSyllables']
+                tags = new_word['tags']
+            except KeyError:
+                continue
+            iterations += 1
+            # print(iterations, new_word)
+    else:
+        while n_1_syl + syllable_count > 10 or part_of_speech not in tags:
+            new_word = word_list_json[random.randrange(len(word_list_json)) - 1]
+            try:
+                n_1_syl = new_word['numSyllables']
+                tags = new_word['tags']
+            except KeyError:
+                continue
+            iterations += 1
+
+            cheating_words = {'n': 'dog', 'v': 'run', 'adj': 'smooth', 'adv': 'hard'}
+            if iterations > 200:
+                new_word = {'word': cheating_words[part_of_speech], 'numSyllables': 1, 'tags': [part_of_speech]}
+                break
+            # print(iterations, new_word)
+
+    # bool_check = part_of_speech in tags
+    # print("part of speech:", part_of_speech, "tags:", tags, "bool:", bool_check)
+    # print("iterations", iterations)
+    return new_word
+
+
+def choose_new_word_for_line_fill_to_ten(part_of_speech, syllable_count):
+    out = proper_line_word_chooser(part_of_speech, syllable_count, True)
+    return out
+
+
+def choose_new_word_for_line_fill(part_of_speech, syllable_count):
+    out = proper_line_word_chooser(part_of_speech, syllable_count, False)
+    return out
+
+
 def fill_line_grammatically(end_word):
     out = ""
+    out_list = []
     syllable_count = end_word['numSyllables']
 
     # get the parts of speech tag from the end word
+    part_of_speech_tags = []
+    for tag in end_word['tags']:
+        if tag == 'n' or tag == 'v' or tag == 'adj' or tag == 'adv':
+            part_of_speech_tags.append(tag)
+    if len(part_of_speech_tags) > 1:
+        part_of_speech = part_of_speech_tags[random.randrange(len(part_of_speech_tags)) - 1]
+    else:
+        part_of_speech = part_of_speech_tags[0]
+
     # noun: fill line as noun-verb-noun and then add adjectives/adverbs to fill syllables
+    if part_of_speech == 'n':
+        new_word = choose_new_word_for_line_fill('n', syllable_count)
+        out_list.append(new_word)
+        syllable_count += new_word['numSyllables']
+
+        new_word = choose_new_word_for_line_fill('v', syllable_count)
+        out_list.append(new_word)
+        syllable_count += new_word['numSyllables']
+
+        if syllable_count < 10:
+            # print(syllable_count)
+            new_word = choose_new_word_for_line_fill_to_ten('adj', syllable_count)
+            out_list.insert(2, new_word)
+            syllable_count += new_word['numSyllables']
+
+            if syllable_count < 10:
+                new_word = choose_new_word_for_line_fill_to_ten('adv', syllable_count)
+                out_list.insert(1, new_word)
+                syllable_count += new_word['numSyllables']
+
+                if syllable_count < 10:
+                    new_word = choose_new_word_for_line_fill_to_ten('adj', syllable_count)
+                    out_list.insert(0, new_word)
+                    syllable_count += new_word['numSyllables']
+
+                    reference = [4, 2, 0]
+                    for i in range(3):
+                        # print(syllable_count)
+                        if syllable_count >= 10:
+                            break
+                        else:
+                            new_word = choose_new_word_for_line_fill_to_ten('adv', syllable_count)
+                            out_list.insert(reference[i], new_word)
+                            syllable_count += new_word['numSyllables']
+
+                    if syllable_count < 10:
+                        # print(syllable_count)
+                        is_was = random.random()
+                        if is_was > 0.5:
+                            out_list.insert(0, {'word': 'is', 'numSyllables': 1, 'tags': ['p']})
+                        else:
+                            out_list.insert(0, {'word': 'was', 'numSyllables': 1, 'tags': ['p']})
+                        syllable_count += 1
+
     # verb: fill line as noun-noun-verb and then add adjectives/adverbs to fill syllables
+    if part_of_speech == 'v':
+        new_word = choose_new_word_for_line_fill('n', syllable_count)
+        out_list.append(new_word)
+        syllable_count += new_word['numSyllables']
+
+        new_word = choose_new_word_for_line_fill('n', syllable_count)
+        out_list.append(new_word)
+        syllable_count += new_word['numSyllables']
+
+        if syllable_count < 10:
+            # print(syllable_count)
+            new_word = choose_new_word_for_line_fill_to_ten('adj', syllable_count)
+            out_list.insert(0, new_word)
+            syllable_count += new_word['numSyllables']
+
+            if syllable_count < 10:
+                new_word = choose_new_word_for_line_fill_to_ten('adv', syllable_count)
+                out_list.insert(3, new_word)
+                syllable_count += new_word['numSyllables']
+
+                if syllable_count < 10:
+                    new_word = choose_new_word_for_line_fill_to_ten('adj', syllable_count)
+                    out_list.insert(2, new_word)
+                    syllable_count += new_word['numSyllables']
+
+                    reference = [4, 2, 0]
+                    for i in range(3):
+                        # print(syllable_count)
+                        if syllable_count >= 10:
+                            break
+                        else:
+                            new_word = choose_new_word_for_line_fill_to_ten('adv', syllable_count)
+                            out_list.insert(reference[i], new_word)
+                            syllable_count += new_word['numSyllables']
+
+                    if syllable_count < 10:
+                        # print(syllable_count)
+                        is_was = random.random()
+                        if is_was > 0.5:
+                            out_list.insert(0, {'word': 'is', 'numSyllables': 1, 'tags': ['p']})
+                        else:
+                            out_list.insert(0, {'word': 'was', 'numSyllables': 1, 'tags': ['p']})
+                        syllable_count += 1
+
     # adjective: fill line as noun-[is/was]-adjective and adjectives/adverbs to fill syllables
+    if part_of_speech == 'adj':
+        new_word = choose_new_word_for_line_fill('n', syllable_count)
+        out_list.append(new_word)
+        syllable_count += new_word['numSyllables']
+
+        is_was = random.random()
+        if is_was > 0.5:
+            out_list.append({'word': 'is', 'numSyllables': 1, 'tags': ['p']})
+        else:
+            out_list.append({'word': 'was', 'numSyllables': 1, 'tags': ['p']})
+        syllable_count += 1
+
+        if syllable_count < 10:
+            # print(syllable_count)
+            new_word = choose_new_word_for_line_fill_to_ten('adj', syllable_count)
+            out_list.insert(0, new_word)
+            syllable_count += new_word['numSyllables']
+
+            if syllable_count < 10:
+                new_word = choose_new_word_for_line_fill_to_ten('adv', syllable_count)
+                out_list.insert(3, new_word)
+                syllable_count += new_word['numSyllables']
+
+                if syllable_count < 10:
+                    new_word = choose_new_word_for_line_fill_to_ten('adv', syllable_count)
+                    out_list.insert(0, new_word)
+                    syllable_count += new_word['numSyllables']
+
+                    reference = [4, 3, 0]
+                    for i in range(3):
+                        # print(syllable_count)
+                        if syllable_count >= 10:
+                            break
+                        else:
+                            new_word = choose_new_word_for_line_fill_to_ten('adv', syllable_count)
+                            out_list.insert(reference[i], new_word)
+                            syllable_count += new_word['numSyllables']
+
+                    if syllable_count < 10:
+                        # print(syllable_count)
+                        is_was = random.random()
+                        if is_was > 0.5:
+                            out_list.insert(0, {'word': 'is', 'numSyllables': 1, 'tags': ['p']})
+                        else:
+                            out_list.insert(0, {'word': 'was', 'numSyllables': 1, 'tags': ['p']})
+                        syllable_count += 1
+
     # adverb: fill line as noun-[[is/was]-adjective/verb]-adverb and add adjectives/adverbs to fill syllables
+    if part_of_speech == 'adv':
+        new_word = choose_new_word_for_line_fill('n', syllable_count)
+        out_list.append(new_word)
+        syllable_count += new_word['numSyllables']
+
+        is_was = random.random()
+        if is_was > 0.5:
+            out_list.append({'word': 'is', 'numSyllables': 1, 'tags': ['p']})
+        else:
+            out_list.append({'word': 'was', 'numSyllables': 1, 'tags': ['p']})
+        syllable_count += 1
+
+        if syllable_count < 10:
+            # print(syllable_count)
+            new_word = choose_new_word_for_line_fill_to_ten('v', syllable_count)
+            out_list.insert(2, new_word)
+            syllable_count += new_word['numSyllables']
+
+            if syllable_count < 10:
+                new_word = choose_new_word_for_line_fill_to_ten('adj', syllable_count)
+                out_list.insert(0, new_word)
+                syllable_count += new_word['numSyllables']
+
+                if syllable_count < 10:
+                    new_word = choose_new_word_for_line_fill_to_ten('adv', syllable_count)
+                    out_list.insert(3, new_word)
+                    syllable_count += new_word['numSyllables']
+
+                    reference = [4, 3, 0]
+                    for i in range(3):
+                        # print(syllable_count)
+                        if syllable_count >= 10:
+                            break
+                        else:
+                            new_word = choose_new_word_for_line_fill_to_ten('adv', syllable_count)
+                            out_list.insert(reference[i], new_word)
+                            syllable_count += new_word['numSyllables']
+
+                    if syllable_count < 10:
+                        # print(syllable_count)
+                        is_was = random.random()
+                        if is_was > 0.5:
+                            out_list.insert(0, {'word': 'is', 'numSyllables': 1, 'tags': ['p']})
+                        else:
+                            out_list.insert(0, {'word': 'was', 'numSyllables': 1, 'tags': ['p']})
+                        syllable_count += 1
+
     # all (at the end or in each section): correct noun and verb pluralization
 
-    out += end_word['word']
+    if not syllable_count == 10:
+        print(f"potential issue, line made with {syllable_count} syllables")
+    out_list.append(end_word)
+    for word in out_list:
+        out += word['word']
+        out += " "
+
+    type_list = []
+    for word in out_list:
+        try:
+            type_list.append(word['tags'])
+        except KeyError:
+            continue
+    # print(type_list)
+    # print(part_of_speech_tags)
     return out
 
 
@@ -174,8 +431,8 @@ def write_line_input(given_word, use_word_passed):
     if use_word_passed is False:
         # accesses the rhyme API for a random word (ensures that there are rhymes)
         layer_two_rhyme_list = []
-        word = ""
-        end_word = ""
+        # word = ""
+        # end_word = ""
         # checks rhyme safety two layers deep
         while len(layer_two_rhyme_list) <= 1 and not rhyme_list_safe(layer_two_rhyme_list):
             word = word_list_json[random.randrange(len(word_list_json)) - 1]["word"]
@@ -184,29 +441,29 @@ def write_line_input(given_word, use_word_passed):
                 word = word_list_json[random.randrange(len(word_list_json)) - 1]["word"]
                 rhyme_list = make_rhyme_list(word)
             if rhyme_list_safe(rhyme_list):
-                print("List is safe, proceeding to layer two checks")
+                # print("List is safe, proceeding to layer two checks")
                 end_word_index = safe_rhyme_word_index(rhyme_list)
                 end_word = rhyme_list[end_word_index]['word']
                 layer_two_rhyme_list = make_rhyme_list(end_word)
             else:
                 continue
 
-        print(f"Finding safe rhyme for {word}")
-        print(f"Rhyme found! ({end_word})")
+        # print(f"Finding safe rhyme for {word}")
+        # print(f"Rhyme found! ({end_word})")
     else:
         word = given_word["word"]
         rhyme_list = make_rhyme_list(word)
 
-        print(f"Finding safe rhyme for {word}")
+        # print(f"Finding safe rhyme for {word}")
         end_word_index = safe_rhyme_word_index(rhyme_list)
         end_word = rhyme_list[end_word_index]['word']
-        print(f"Rhyme found! ({end_word})")
+        # print(f"Rhyme found! ({end_word})")
 
     total_syl += rhyme_list[end_word_index]['numSyllables']
 
     out = fill_line_grammatically(rhyme_list[end_word_index])
 
-    print(f"Finished creating new line: {out}")
+    # print(f"Finished creating new line: {out}")
     global file_output
     file_output = file_output + out + "\n"
 
@@ -214,31 +471,75 @@ def write_line_input(given_word, use_word_passed):
 
 
 def main(iterations):
+    start = datetime.now()
     word_list_base = make_word_list()
     sonnet_list = []
     global file_output
-    for k in range(iterations):
-        if k % 100 == 0:  # changes the seed every 100 poems
+    for i in range(iterations):
+        if i % 100 == 0:  # changes the seed every 100 poems
+            print("Finding new theme for next 100 poems...")
             seed = word_list_base[random.randrange(len(word_list_base)) - 1]
             global word_list_json
-            word_list_json = make_line_fill_json(seed, True)  # TODO remember to set this to false
-        for i in range(3):
-            rhyme_one = write_line()
-            rhyme_two = write_line()
-            write_line_input(rhyme_one, True)
-            write_line_input(rhyme_two, True)
-            file_output = file_output + "\n"
+            word_list_json = make_line_fill_json(seed, False)  # set to true for debugging
+            print("Theme found and successfully loaded!\n")
+
+        print(f"Writing sonnet #{i + 1}:")
+
         rhyme_one = write_line()
+        print("1/14 lines complete")
+        rhyme_two = write_line()
+        print("2/14 lines complete")
         write_line_input(rhyme_one, True)
+        print("3/14 lines complete")
+        write_line_input(rhyme_two, True)
+        print("4/14 lines complete")
+        file_output = file_output + "\n"
+        rhyme_one = write_line()
+        print("5/14 lines complete")
+        rhyme_two = write_line()
+        print("6/14 lines complete")
+        write_line_input(rhyme_one, True)
+        print("7/14 lines complete")
+        write_line_input(rhyme_two, True)
+        print("8/14 lines complete")
+        file_output = file_output + "\n"
+        rhyme_one = write_line()
+        print("9/14 lines complete")
+        rhyme_two = write_line()
+        print("10/14 lines complete")
+        write_line_input(rhyme_one, True)
+        print("11/14 lines complete")
+        write_line_input(rhyme_two, True)
+        print("12/14 lines complete")
+        file_output = file_output + "\n"
+        rhyme_one = write_line()
+        print("13/14 lines complete")
+        write_line_input(rhyme_one, True)
+        print("14/14 lines complete \n")
         file_output = "\n" + file_output
         sonnet_list.append(file_output)
         file_output = ""
-        with open(f'sonnet_out_#{k}.txt', 'w') as Writer:
-            Writer.write(f"Sonnet #{k}")
-            Writer.write("\n")
-            Writer.write(sonnet_list[k])
+        with open(f'sonnet_out_#{i + 1}.txt', 'w') as Writer:
+            Writer.write(f"Sonnet #{i + 1} \n")
+            Writer.write(sonnet_list[i])
+    end = datetime.now()
+
+    total_time = end - start
+    time_per_poem = total_time.total_seconds() / iterations
+    poems_per_hour = round(3600 / time_per_poem)
+    print(f'{iterations} sonnet(s) written in {total_time} \n({time_per_poem} seconds per poem) '
+          f'\n(about {poems_per_hour} poems per hour)')
+
+    with open('sonnet_out_all.txt', 'w') as Writer:
+        Writer.write(f'{iterations} sonnet(s) written in {total_time} \n({time_per_poem} seconds per poem) '
+                     f'\n(about {poems_per_hour} poems per hour)')
+        Writer.write("\n-------------------------------------------------\n")
+        for i in range(len(sonnet_list)):
+            Writer.write(f"Sonnet #{i+1} \n")
+            Writer.write(sonnet_list[i])
+            Writer.write("\n-------------------------------------------------\n")
 
 
 if __name__ == "__main__":
     # make_test_json()  # not necessary except for debugging purposes
-    main(10)
+    main(1000)
